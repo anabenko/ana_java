001a88788fd18895bb07f29d612b090a5a56996e
anabenko <maybeana@gmail.com>
2017-03-22 15:26:24 +0200

    added few functions

--- sandbox/src/main/java/ana/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ana/test/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ana.test.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
 
-    System.out.println("hello, world!");
-  }
+    hello("ana");
+    double l = 5;
+    System.out.println("area of " + l + " is " + area(l));
 
+    double a = 2;
+    double b = 4;
+    System.out.println("rectangular square of " + a + " and " + b + " is " + area(a, b));
+
+  }
+public static void hello (String name) {
+    System.out.println("hello " + name +"!");
+}
+
+public static double area (double len){
+    return len*len;
+}
+
+public static double area (double a, double b) {
+  return a*b;
+}
 }
